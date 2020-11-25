4a02dd580936e1e573830fbb9af833a4ea5993e7
Sakuraba_Jun <nyantan1918@gmail.com>
2020-11-25 19:01:01 +0900

    プログラムワークショップⅣ第10回提出課題

---
+++ src/Assets/NewUnlitShader.shader
@@ -0,0 +1,74 @@
+﻿Shader "Unlit/NewUnlitShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Mix("Mix",Range(0,1)) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+        LOD 100
+        Blend SrcAlpha OneMinusSrcAlpha
+        cull Off
+
+        GrabPass{"_backgroundTexture"}
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 grabPos : TEXCOORD2;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            sampler2D _backgroundTexture;
+            float4 _MainTex_ST;
+            float _Mix;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                o.grabPos = ComputeGrabScreenPos(o.vertex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                fixed4 decal = tex2D(_MainTex,i.uv);
+                // sample the texture
+                fixed4 col = tex2Dproj(_backgroundTexture, i.grabPos
+                    + 100.0
+                    * (decal.a + 0.5)
+                    * float4(ddx(i.uv.x), ddy(i.uv.x),0, 0)
+                );
+                col.a = decal.a;
+
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col + (decal * _Mix);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ src/Assets/TrailController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class TrailController : MonoBehaviour
+{
+    public int FLAME_MAX = 10;
+    public int DIVIDE_MAX = 4;
+    private List<Vector3> point0 = new List<Vector3>();
+    private List<Vector3> point1 = new List<Vector3>();
+
+    private Mesh mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FLAME_MAX <= point0.Count)
+        {
+            point0.RemoveAt(0);
+            point1.RemoveAt(0);
+        }
+
+        point0.Add(transform.position);
+        point1.Add(transform.TransformPoint(new Vector3(0.0f, 1.0f, 0.0f)));
+
+        if (2 <= point0.Count) 
+        {
+            Render();
+        }
+    }
+
+    void Render()
+    {
+        mesh.Clear();
+
+        int n = point0.Count;
+        Vector3[] vertexArray = new Vector3[2 * (n - 1) * DIVIDE_MAX + 2];
+        Vector2[] uvArray = new Vector2[2 * (n - 1) * DIVIDE_MAX + 2];
+        int[] indexArray = new int[6 * (n - 1) * DIVIDE_MAX];
+
+        int idv = 0, idi = 0;
+        float duv = 1.0f / ((float)DIVIDE_MAX * ((float)n - 1.0f));
+
+        int id0 = 0, id1 = 0, id2 = 1, id3 = 2;
+        if (n - 1 < id3) id3 = n - 1;
+
+
+        for (int i = 0; i < n - 1; i++) 
+        {
+            for(int j = 0; j < DIVIDE_MAX; j++)
+            {
+
+                float t = (float)j / (float)DIVIDE_MAX;
+                Vector3 p0 = CatmullRom(point0[id0], point0[id1], point0[id2], point0[id3], t);
+                Vector3 p1 = CatmullRom(point1[id0], point1[id1], point1[id2], point1[id3], t);
+
+                vertexArray[idv + 0] = transform.InverseTransformPoint(p0);
+                vertexArray[idv + 1] = transform.InverseTransformPoint(p1);
+
+                uvArray[idv + 0].x = uvArray[idv + 1].x = 1.0f - duv * (float)(i * DIVIDE_MAX + j);
+                uvArray[idv + 0].y = 0.0f;
+                uvArray[idv + 1].y = 1.0f;
+
+                indexArray[idi + 0] = idv + 0;
+                indexArray[idi + 1] = idv + 1;
+                indexArray[idi + 2] = idv + 2;
+                indexArray[idi + 3] = idv + 2;
+                indexArray[idi + 4] = idv + 1;
+                indexArray[idi + 5] = idv + 3;
+
+                idv += 2;
+                idi += 6;
+            }
+
+            if (i != 0) id0++;
+            if (n <= ++id1) id1 = n - 1;
+            if (n <= ++id2) id2 = n - 1;
+            if (n <= ++id3) id3 = n - 1;
+        }
+
+        vertexArray[idv + 0] = transform.InverseTransformPoint(point0[n - 1]);
+        vertexArray[idv + 1] = transform.InverseTransformPoint(point1[n - 1]);
+
+        uvArray[idv + 0].x = uvArray[idv + 1].x = 0.0f;
+        uvArray[idv + 0].y = 0.0f;
+        uvArray[idv + 1].y = 1.0f;
+
+        mesh.vertices = vertexArray;
+        mesh.uv = uvArray;
+        mesh.triangles = indexArray;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
+    Vector3 CatmullRom(in Vector3 p0, in Vector3 p1, in Vector3 p2, in Vector3 p3, float t)
+    {
+        float t2 = t * t;
+        float t3 = t * t2;
+        float c0 = -0.5f * t3 + t2 - 0.5f * t;
+        float c1 = +1.5f * t3 - 2.5f * t2 + 1.0f;
+        float c2 = -1.5f * t3 + 2.0f * t2 + 0.5f * t;
+        float c3 = +0.5f * t3 - 0.5f * t2;
+
+        return p0 * c0 + p1 * c1 + p2 * c2 + p3 * c3;
+    }
+}
